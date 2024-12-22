870dcd64d8483aa3a85618f34468efbc5e76bc7b
Ang2Tea <angtwotea@gmail.com>
2024-12-23 02:05:12 +0300

    feat: Add di and auto migration for project

--- src/AccountingSystem.Web/Program.cs
+++ src/AccountingSystem.Web/Program.cs
@@ -1,9 +1,18 @@
+using AccountingSystem.EntityFramework.EntityFrameworkCore;
+using AccountingSystem.Web;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AccountingSystemDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("AccountingSystem")));
 builder.Services.AddControllersWithViews();
+builder.Services.Di();
 
 var app = builder.Build();
+
+app.Migrate<AccountingSystemDbContext>();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -12,7 +21,6 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
