07e681522e5d2f3ebe1fe13ee6400331b998ad6e
Ang2Tea <angtwotea@gmail.com>
2024-12-24 00:52:38 +0300

    feat: Fix conflicts

--- src/AccountingSystem.Web/Program.cs
+++ src/AccountingSystem.Web/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddDbContext<AccountingSystemDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("AccountingSystem")));
-builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
 builder.Services.Di();
 
 var app = builder.Build();
@@ -17,10 +17,11 @@ app.Migrate<AccountingSystemDbContext>();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
+    app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
@@ -28,8 +29,6 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
 
 app.Run();
