62fc8453f077dfc115a84de87dbd3a1113296fea
Ang2Tea <angtwotea@gmail.com>
2024-12-24 01:30:12 +0300

    wip

---
+++ src/AccountingSystem.Web/Pages/ShopItems/Index.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AccountingSystem.Web.Pages.ShopItems;
+
+public class Index : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
