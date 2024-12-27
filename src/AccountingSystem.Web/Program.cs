e90f30aa730444a6b2768df21b9e9d9f8d5f38c1
Andrey Ganya <a.ganya@appricot.ru>
2024-12-27 09:33:28 +0300

    feat: Add role levels for page

--- src/AccountingSystem.Web/Program.cs
+++ src/AccountingSystem.Web/Program.cs
@@ -1,6 +1,9 @@
+using System.Text;
 using AccountingSystem.EntityFramework.EntityFrameworkCore;
 using AccountingSystem.Web;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,10 +11,36 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AccountingSystemDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("AccountingSystem")));
 
-builder.Services.AddRazorPages();
+builder.Services.AddRazorPages(options =>
+{
+    options.Conventions.AuthorizeFolder("/Categories", "admin");
+    options.Conventions.AuthorizeFolder("/ShopItems", "admin");
+    options.Conventions.AuthorizeFolder("/Transactions", "admin");
+    options.Conventions.AllowAnonymousToPage("/Index");
+    options.Conventions.AllowAnonymousToFolder("/Auth");
+} );
 builder.Services.Di();
 
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+{
+    var config = builder.Configuration["PrivateKey"];
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config)),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.Migrate<AccountingSystemDbContext>();
 
