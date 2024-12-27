a4ba0f0da269c4b181b1ae19afa2db7409f5ce3e
Ang2Tea <angtwotea@gmail.com>
2024-12-27 03:33:34 +0300

    feat: Add migrations

--- src/AccountingSystem.EntityFramework/Migrations/20241227003156_Add_User_Roles.Designer.cs
+++ src/AccountingSystem.EntityFramework/Migrations/20241227003156_Add_User_Roles.Designer.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using AccountingSystem.EntityFramework.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -12,9 +13,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace AccountingSystem.EntityFramework.Migrations
 {
     [DbContext(typeof(AccountingSystemDbContext))]
-    partial class AccountingSystemDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241227003156_Add_User_Roles")]
+    partial class Add_User_Roles
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/AccountingSystem.EntityFramework/Migrations/20241227003156_Add_User_Roles.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AccountingSystem.EntityFramework.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_User_Roles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsAdmin",
+                table: "Users");
+
+            migrationBuilder.AddColumn<List<string>>(
+                name: "Roles",
+                table: "Users",
+                type: "text[]",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Roles",
+                table: "Users");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAdmin",
+                table: "Users",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
