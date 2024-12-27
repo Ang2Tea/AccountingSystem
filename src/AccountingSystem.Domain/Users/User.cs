9ddb43154a243500a4b0d31001373b510a715648
Ang2Tea <angtwotea@gmail.com>
2024-12-27 03:32:26 +0300

    feat: Change roles

--- src/AccountingSystem.Domain/Users/User.cs
+++ src/AccountingSystem.Domain/Users/User.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using AccountingSystem.Domain.Core;
 
@@ -11,50 +12,39 @@ namespace AccountingSystem.Domain.Users
 
         public string Email { get; private set; }
 
-        public bool IsAdmin { get; private set; }
+        public List<string> Roles { get;  set; }
 
         public string Name { get; set; }
         public string Surname { get; set; }
 
         public User() { }
         public User(Guid id) : base(id) { }
+
         public User(string login, string password, string salt, string email, string name, string surname)
         {
             Login = login;
-            Password = GeneratePasswordHash(password, salt);
+            Password = password;
             Email = email;
-            IsAdmin = false;
+            Roles = new List<string>();
             Name = name;
             Surname = surname;
         }
 
-        private static string GeneratePasswordHash(string password, string salt)
-        {
-            return "";
-        }
-        
         private Task ChangeAdmin(bool isAdmin)
         {
-            IsAdmin = isAdmin;
+            if (isAdmin)
+            {
+                Roles.Add("admin");
+            }
+            else
+            {
+                Roles.Remove("admin");
+            }
+            
             return Task.CompletedTask;
         }
 
         public Task OnAdminStatus() => ChangeAdmin(true);
         public Task OffAdminStatus() => ChangeAdmin(false);
-        
-        public async Task ChangePassword(string oldPassword, string newPassword, string salt)
-        {
-            var oldPasswordHash = Task.Run (() => GeneratePasswordHash(oldPassword, salt));
-            var newPasswordHash = Task.Run(() => GeneratePasswordHash(newPassword, salt));
-            
-            var passwordHashes = await Task.WhenAll(oldPasswordHash, newPasswordHash);
-
-            if (passwordHashes[0] != Password || passwordHashes[0] == passwordHashes[1])
-            {
-                throw new Exception("Passwords do not match");
-            }
-            
-            Password = passwordHashes[1];
-        }
     }
 }
